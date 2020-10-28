5fe1b580bad4b4710a9b166b6bf0b9991eebaeea
satwicksam <73584472+satwicksam@users.noreply.github.com>
2020-10-28 20:04:29 +0530

    Update max-sum.cpp

--- max-sum.cpp
+++ max-sum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define rec(a,i,n) for(int i=a;i<n;i++)
 main()
 {
-int arr[]={-1,2,4,-3,5,2,-5,2};
+int arr[]={-1,2,5,-3,5,2,-5,2};
 int n=*(&arr+1)-arr;
 int best=0;
 for (int a = 0; a < n; a++) {
@@ -13,6 +13,6 @@ sum += arr[b];
 best = max(best,sum);
 }
 }
-cout<<"The maximum sum of the elements is "<<best;
+cout<<"The is "<<best;
 return 0;
 }
